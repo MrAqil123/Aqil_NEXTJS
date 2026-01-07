372ec5c452f525fe64a6a509499f3547cbd927e7
MrAqil123 <Mraqil1236@gmail.com>
2026-01-08 01:55:08 +0430

    fix(tailwind): correct globals, postcss and add commonjs tailwind config for Vercel compatibility

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './page.tsx',
+    './src/app/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './markdown/**/*.{md,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,md,mdx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
